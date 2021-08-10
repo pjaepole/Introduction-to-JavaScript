78f031dac4a93c4d45c4940dec5f73846cd01788
Jaehun Park <pjaehun4122@gmail.com>
2021-08-10 15:05:20 -0700

    finished project but i dont get it so im in after hour

--- index.js
+++ index.js
@@ -169,9 +169,28 @@ RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors
 
 HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
+let computer = Math.random();
+
+if(computer<= 0.34){
+  computer = 'rock'
+}else if (computer<= 0.67){
+  computer = 'paper';
+}else if (computer >0.67){
+  computer = 'scissors';
+}
 
 function game(user, computer){
-  /*add your code here*/
+  if (user === computer){
+    return `it's a tie`;
+  }else if (user === 'rock' && computer === 'scissors'){
+    return `you win!`;
+  }else if (user === 'paper' && computer === 'rock'){
+    return `you win!`;
+  }else if (user === 'scissors' && computer === 'paper'){
+    return `you win!`
+  }else{
+    return `you lose!`;
+  }
 }
 
 
@@ -187,11 +206,11 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return km * 0.621371;
 }
 
-
+console.log(miles(2))
 
 //Task 5b - Feet to CM
 /*
@@ -201,10 +220,11 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48;
 }
 
+console.log(feet(2))
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -214,13 +234,15 @@ function feet(/*add your code here*/){
 Using the annoyingSong function below do the following:
 1. Receive a starting number and start the count down from the number received 
 2. At each invocation, it should RETURN this string (note: the tests are expecting the same string as below):
-    "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
+    "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number } bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(number){
+      for(let i = number; i > 0; i--){
+        return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i-1} bottles of soda on the wall`;
+      }
 }
-
+console.log(annoyingSong(11))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -237,8 +259,18 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+    if(score >= 90){
+      return `you got an A`
+    }else if (score <90 && score >=80){
+      return `you got a B`
+    }else if (score <80 && score >=70){
+      return `you got a C`
+    }else if (score <70 && score >=60){
+      return `you got a D`
+    }else if (score <60){
+      return `you got an F`}
+
 }
 
 
